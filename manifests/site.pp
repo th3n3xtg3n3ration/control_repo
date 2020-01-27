8eb8319fa7cfbdedb6fc991cc96605fbe9f7f667
Ugur Celebi <hasanugurcelebi@yahoo.com>
2020-01-27 20:31:04 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,10 +1,3 @@
 node default {
-  file  {'/root/README':
-    ensure  =>  file,
-    content => 'This is a readme',
-    owner   => 'root',
-  }
-  file {'/root/README':
-    owner   => 'root',
-  }
+ 
 }
