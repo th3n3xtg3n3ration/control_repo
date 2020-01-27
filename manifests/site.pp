01e3bf81060bdd7d29e79aaa5dfdf60f9583a7a4
Ugur Celebi <hasanugurcelebi@yahoo.com>
2020-01-28 00:42:38 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -5,11 +5,9 @@ node 'master.puppet.vm' {
    include role::master_server
 }
 
-node /^web/ {
-   include role::app_server
+node /^web/ { 
+  include role::app_server
 }
-
 node /^db/ {
-   include role::db_server
+  include role::db_server
 }
-
