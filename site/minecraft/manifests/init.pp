11cea3ebe16947d512eea110326456aba785ac3d
Ugur Celebi <hasanugurcelebi@yahoo.com>
2020-02-01 01:09:39 +0300

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -19,7 +19,9 @@ class minecraft (
   }
   file {'/etc/systemd/system/minecraft.service':
     ensure => file,
-    source => 'puppet:///modules/minecraft/minecraft.service',
+    source => epp('minecraft/minecraft.service', {
+    install_dir => $install_dir,
+    })
   }
   service { 'minecraft':
     ensure => running,
